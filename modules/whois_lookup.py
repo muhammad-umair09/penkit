69e44f8f501a75d406515ec85c3d1cd8f8f096b8
Muhammad Umair <muhammadumair1793@gmail.com>
2026-07-07 09:24:05 +0500

    first commit

--- modules/whois_lookup.py
+++ modules/whois_lookup.py
@@ -2,12 +2,17 @@ import whois
 
 def run_whois(domain):
     try:
-        data = whois.whois(domain)
+        data = whois.query(domain)
 
-        print("Domain:", data.domain_name)
+        if data is None:
+            print("No WHOIS information found.")
+            return
+
+        print("Domain:", data.name)
         print("Registrar:", data.registrar)
         print("Creation:", data.creation_date)
         print("Expiry:", data.expiration_date)
+        print("Name Servers:", data.name_servers)
 
     except Exception as e:
         print(f"Error: {e}")
